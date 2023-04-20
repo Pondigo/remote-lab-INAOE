725fe4343d78c5eb3e5dec60c183094ef73d0dbb
Pondigo <lenora.pondigo@gmail.com>
2023-04-19 18:51:17 -0600

    Init remote control for logic gate

--- remote_lab_api/remote_lab/logic_gate/logic_gate.py
+++ remote_lab_api/remote_lab/logic_gate/logic_gate.py
@@ -1,8 +1,65 @@
 from fastapi import APIRouter
+from pyvisa import ResourceManager
 
 router = APIRouter()
+rm = ResourceManager()
+#Get available resources
+available_resources = rm.list_resources()
+
+#Get AWG device
+awg = rm.open_resource("USB0::0x0699::0x0353::2152273::INSTR")
+#Get OSC device
+osc = rm.open_resource("USB0::0x0699::0x03C4::C010900::INSTR")
+
+#Reset OSC
+osc.write("*RST")
+
+#Set OSC timeout as 20
+osc.timeout = 20000
+osc.write("HORIZONTAL:MAIN:SCALE 1")
+osc.write("CH1:SCALE 5")
+osc.write("CH2:SCALE 5")
+
 
 
 @router.get("/")
 async def read_logic_gate():
-    return {"message": "logic gate sub-API!"}
+    #Print available resources with their info (IDN)
+    osc.write("SELECT:CH1 ON;CH2 ON")
+    osc.write("DATA:ENCDG RIBINARY")
+    awg.write("SOURce1:FUNCtion:SHAPe SQUare")
+    awg.write("SOURce2:FUNCtion:SHAPe SQUare")
+    awg.write("SOURce1:FREQuency:FIXed 0.2Hz")
+    awg.write("SOURce2:FREQuency:FIXed 0.4Hz")
+
+
+    awg.write("OUTPut1:STATe ON;OUTPut2:STATe ON")
+    res = waitOsc()
+    print("res: ", res)
+    awg.write("OUTPut1:STATe OFF;OUTPut2:STATe OFF")
+
+    return res
+
+#Define waitOsc function
+def waitOsc():
+    osc.write("ACQUIRE:STATE OFF")
+    osc.write("SELECT:CH1 ON")
+    osc.write("ACQUIRE:MODE SAMPLE")
+    osc.write("ACQUIRE:STOPAFTER SEQUENCE")
+    osc.write("ACQUIRE:STATE ON")
+    osc.write("MEASUREMENT:IMMED:TYPE AMPLITUDE")
+    osc.write("MEASUREMENT:IMMED:SOURCE CH1")
+    osc.write("*WAI")
+    return getCh1()
+
+
+
+
+def getCh1():
+    osc.write("DATA:SOURCE CH1")
+    #Get channel 1 data CURVE?
+    ch1 = osc.query_binary_values("CURVE?", datatype='B', is_big_endian=True)
+
+    #print("ch1: ", ch1)
+    return ch1
+
--- remote_lab_gui/src/App.tsx
+++ remote_lab_gui/src/App.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import mosfetIcon from './icons/mosfet-icon.png'
 import nandIcon from './icons/nand-icon.png'
 import solarCellIcon from './icons/solar-cell.png'
+import axios from "axios";
 
 //Get API_URL from .env
 const API_URL = process.env.REACT_APP_API_URL;
@@ -25,6 +26,13 @@ const getTitleFromPath = (path: string) => {
   }
 };
 
+//Axios get to http://localhost:8000/remote_lab/logic_gate
+const fetchLogicGate = async () => {
+  const res = await axios.get("http://localhost:8000/remote_lab/logic_gate");
+  console.log(res.data);
+};
+  
+
 export default function App() {
   const [title, setTitle] = useState<string>("");
   const location = useLocation();
@@ -39,48 +47,65 @@ export default function App() {
     <>
       <Header title={title} />
 
-      <Routes >
-        <Route path="/" element={<div>
-          <div className="h-full w-full">
-            <div style={{ height: "85vh" }} className="flex flex-wrap justify-center mt-4">
-              <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
-                <button className="w-full h-full bg-inaoe_comp2 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col"
-                onClick={()=>navigate("/solarCell")}>
-                  <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
-                    Celda Solar
-                    <img className="w-1/3" src={solarCellIcon} alt="solar-cell-icon" />
-                  </span>
-                </button>
-              </div>
-              <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
-                <button className="w-full h-full bg-inaoe_comp3 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col relative">
-                <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
-                    MOSFET
-                    <img className="w-1/3" src={mosfetIcon} alt="mosfet-icon" />
-                  </span>
-                </button>
-              </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="h-full w-full">
+                <div
+                  style={{ height: "85vh" }}
+                  className="flex flex-wrap justify-center mt-4"
+                >
+                  <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
+                    <button
+                      className="w-full h-full bg-inaoe_comp2 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col"
+                      onClick={() => navigate("/solarCell")}
+                    >
+                      <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
+                        Celda Solar
+                        <img
+                          className="w-1/3"
+                          src={solarCellIcon}
+                          alt="solar-cell-icon"
+                        />
+                      </span>
+                    </button>
+                  </div>
+                  <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
+                    <button className="w-full h-full bg-inaoe_comp3 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col relative">
+                      <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
+                        MOSFET
+                        <img
+                          className="w-1/3"
+                          src={mosfetIcon}
+                          alt="mosfet-icon"
+                        />
+                      </span>
+                    </button>
+                  </div>
 
-              <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
-                <button className="w-full h-full bg-inaoec1 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col">
-                <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
-                    Compuerta Logica NAND
-                    <img className="w-1/3" src={nandIcon} alt="nand-icon" />
-                  </span>
-
-                </button>
-
+                  <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
+                    <button
+                      onClick={fetchLogicGate}
+                      className="w-full h-full bg-inaoec1 hover:bg-opacity-80 text-center font-bold rounded-lg flex flex-col"
+                    >
+                      <span className="flex-1 flex items-center justify-center text-white text-xl flex-col gap-y-10">
+                        Compuerta Logica NAND
+                        <img className="w-1/3" src={nandIcon} alt="nand-icon" />
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-
-        </div>} />
+          }
+        />
         <Route path="/solarCell" element={<SolarCellPage />} />
         <Route path="/*" element={<div>Not found</div>} />
       </Routes>
     </>
-  )
+  );
   //return requireAuth(SolarCellPage)
 }
 
